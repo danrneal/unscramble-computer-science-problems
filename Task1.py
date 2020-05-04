3237e8fa1b561ecbb2821da04d3589f8ede2cc85
Daniel R. Neal <dan.r.neal@gmail.com>
2020-05-03 17:45:45 -0700

    feat: Complete Task1

---
+++ Task1.py
@@ -0,0 +1,29 @@
+"""Read file into texts and calls.
+
+Print the number of unique telephone numbers in the records
+"""
+
+import csv
+
+numbers = set()
+
+
+with open("texts.csv", "r") as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+    for text in texts:
+        numbers.add(text[0])
+        numbers.add(text[1])
+
+
+with open("calls.csv", "r") as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+    for call in calls:
+        numbers.add(call[0])
+        numbers.add(call[1])
+
+
+print(f"There are {len(numbers)} different telephone numbers in the records.")
