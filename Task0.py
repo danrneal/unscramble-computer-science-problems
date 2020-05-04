2e19b503e0d3d13978b346b2dcd7df9d68334ba6
Daniel R. Neal <dan.r.neal@gmail.com>
2020-05-03 17:45:34 -0700

    feat: Complete Task0

---
+++ Task0.py
@@ -0,0 +1,27 @@
+"""Read file into texts and calls.
+
+Print the first record of texts and the last record of calls
+"""
+
+import csv
+
+with open("texts.csv", "r") as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+    incoming_number, answering_number, time = texts[0]
+
+    print(
+        f"First record of texts, {incoming_number} texts {answering_number} "
+        f"at time {time}"
+    )
+
+
+with open("calls.csv", "r") as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+    incoming_number, answering_number, time, duration = calls[-1]
+
+    print(
+        f"Last record of calls, {incoming_number} calls {answering_number} at "
+        f"time {time}, lasting {duration} seconds"
+    )
